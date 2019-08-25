782c75c43a4afc643449044f92b102dce1c87b71
Shane Finlayson <shane_finlayson@yahoo.com>
2019-08-25 15:19:03 -0600

    setup

--- client-side/src/config/url-config.js
+++ client-side/src/config/url-config.js
@@ -1,3 +1,3 @@
-const url = process.env.NODE_ENV === "production" ? `https://wicked-ways.herokuapp.com/` : `http://localhost:8080`;
+const url = process.env.NODE_ENV === "production" ? `https://wicked-ways.herokuapp.com` : `http://localhost:8080`;
 
 export default url;
