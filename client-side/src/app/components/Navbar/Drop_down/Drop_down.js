918fa568d9673b109ab830445a0107f4b1cab713
Shane Finlayson <shane_finlayson@yahoo.com>
2019-09-03 01:51:44 -0600

    Fixed navbar bug

--- client-side/src/app/components/Navbar/Drop_down/Drop_down.js
+++ client-side/src/app/components/Navbar/Drop_down/Drop_down.js
@@ -5,21 +5,26 @@ export default function DropDown(props) {
 
     if (props.menuState) {
         return (
-            <nav id="drop-down">
+            <div id="drop-down-container">
+                <nav id="drop-down">
 
-                <NavLink to="/admin/add-product">
-                    <h3 onClick={props.toggleMenu}>Add Product</h3>
-                </NavLink>
+                    <NavLink to="/admin/add-product">
+                        <h3 onClick={props.toggleMenu}>Add Product</h3>
+                    </NavLink>
 
-                <NavLink to="/contact">
-                    <h3 onClick={props.toggleMenu}>Contact</h3>
-                </NavLink>
+                    <NavLink to="/contact">
+                        <h3 onClick={props.toggleMenu}>Contact</h3>
+                    </NavLink>
 
-                <NavLink to="/about">
-                    <h3 onClick={props.toggleMenu}>About</h3>
-                </NavLink>
+                    <NavLink to="/about">
+                        <h3 onClick={props.toggleMenu}>About</h3>
+                    </NavLink>
 
-            </nav>
+                    <small onClick={props.toggleMenu}>X</small>
+
+                </nav>                
+            </div>
+
         )
     } else {
         return null
--- client-side/src/app/components/Navbar/Navbar.js
+++ client-side/src/app/components/Navbar/Navbar.js
@@ -43,7 +43,7 @@ class Navbar extends React.Component {
             </div>
 
 
-        );
+        ); 
     }
 
 }
